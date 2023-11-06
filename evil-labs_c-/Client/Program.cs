d7a8df43a50c3476eeed915829b5367bec839f1c
RedCross321 <115776446+RedCross321@users.noreply.github.com>
2023-11-06 12:55:31 +0500

    Add files via upload

---
+++ evil-labs_c-/Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+namespace Client;
+
+class Program
+{
+
+    public struct Ad
+    {
+        public int X;
+        public bool Podtv;
+    }
+
+    public static void Main()
+    {
+        try
+        {
+
+            Console.WriteLine("Соединяю с базой\n");
+
+            var stream = new NamedPipeClientStream(".", "tonel", PipeDirection.InOut);
+            stream.Connect();
+            Console.WriteLine("Соединил, шеф!\n");
+
+            Console.WriteLine("Ждем-с данных\n");
+
+            while (true)
+            {
+                byte[] array = new byte[Unsafe.SizeOf<Ad>()];
+                stream.Read(array);
+                var answer = MemoryMarshal.Read<Ad>(array);
+
+                Console.WriteLine($"Получил: {answer.X}, {answer.Podtv}\n");
+                answer.Podtv = true;
+                Console.WriteLine($"Отправил {answer.X}, {answer.Podtv}...\n");
+
+                byte[] spam = new byte[Unsafe.SizeOf<Ad>()];
+                MemoryMarshal.Write<Ad>(spam, ref answer);
+                stream.Write(spam);
+            }
+        }
+        catch
+        {
+
+        }
+
+    }
+    
+}
---
+++ evil-labs_c-/Server/Program.cs
@@ -0,0 +1,131 @@
+﻿using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+namespace Server;
+
+internal class Program
+{
+
+    static CancellationTokenSource up = new CancellationTokenSource();
+    static CancellationToken dawn = up.Token;
+
+    static PriorityQueue<Ad, int> queue = new PriorityQueue<Ad, int>();
+
+
+    static Mutex mutex = new Mutex();
+
+    public struct Ad
+    {
+
+        public int X;
+
+        public bool Podtv;
+
+        public override string ToString() => $"Данные = {X}, Ответ = {Podtv}";
+    }
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Жду клиентика\n");
+        var stream = new NamedPipeServerStream("tonel", PipeDirection.InOut);
+
+
+
+        var clientTask = Task.Run(() => Client(stream, dawn));
+        var serverTask = Task.Run(() => Server(stream, dawn));
+
+
+        Console.CancelKeyPress += (s, e) =>
+        {
+            e.Cancel = true;
+            up.Cancel();
+        };
+
+        await Task.WhenAll(serverTask, clientTask);
+       // Console.WriteLine("Всё");
+
+    }
+
+    static async void Server(NamedPipeServerStream stream, CancellationToken cancellationToken)
+    {
+        stream.WaitForConnection();
+        Console.WriteLine("Клиент подключен!\n");
+
+
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            Console.WriteLine($"Введите значение -> ");
+            var value = Console.ReadLine();
+            if (value.Length == 0)
+            {
+                Console.WriteLine("Ты не ввел цифры, попробуй заново\n");
+                continue;
+            }
+
+
+            Console.WriteLine($"Введите приоритет -> ");
+            var priority = Console.ReadLine();
+            if (priority.Length == 0)
+            {
+                Console.WriteLine("Ты не ввел цифры, попробуй заново\n");
+                continue;
+            }
+
+
+
+            var data = new Ad() { X = Convert.ToInt32(value), Podtv = false };
+            //Console.WriteLine($"Отправляю {data.X}, {data.Podtv}\n");
+            
+
+            
+
+            mutex.WaitOne();
+            queue.Enqueue(data, Convert.ToInt32(priority));
+            mutex.ReleaseMutex();
+
+        }
+
+        await Task.Delay(1000);
+    }
+    static async void Client(NamedPipeServerStream stream, CancellationToken cancellationToken)
+    {
+     
+        List<Ad> uds = new List<Ad>();
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            if (queue.Count >= 1)
+            {
+                /*
+                mutex.WaitOne();
+                var data = queue.Dequeue();
+                mutex.ReleaseMutex();
+                byte[] spam = new byte[Unsafe.SizeOf<Ad>()];
+                MemoryMarshal.Write<Ad>(spam, ref data);
+                stream.Write(spam);
+                byte[] array = new byte[Unsafe.SizeOf<Ad>()];
+                stream.Read(array);
+                uds.Add(MemoryMarshal.Read<Ad>(array));
+                */
+            }
+        }
+        while (queue.Count > 0)
+        {
+            var data = queue.Dequeue();
+            byte[] spam = new byte[Unsafe.SizeOf<Ad>()];
+            MemoryMarshal.Write<Ad>(spam, ref data);
+            stream.Write(spam);
+            byte[] array = new byte[Unsafe.SizeOf<Ad>()];
+            stream.Read(array);
+            uds.Add(MemoryMarshal.Read<Ad>(array));
+
+        }
+        foreach (var item in uds)
+        {
+            Console.WriteLine(item);
+        }
+        await Task.Delay(1000);
+    }
+}
