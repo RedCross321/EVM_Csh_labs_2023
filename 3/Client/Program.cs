305f88028b81b3a1d8a2195f67045b052c0f9438
RedCross321 <115776446+RedCross321@users.noreply.github.com>
2023-12-09 15:30:44 +0500

    Update Program.cs

--- 3/Client/Program.cs
+++ 3/Client/Program.cs
@@ -23,29 +23,26 @@ class Program
             int i;
             var name = args[0];
             var stream = new NamedPipeClientStream(".", name, PipeDirection.InOut);
-            stream.Connect();
-            while (true)
+            await streamAsync.Connect();
+            byte[] array = new byte[Unsafe.SizeOf<Ad>()];
+            stream.ReadAsync(array);
+            var answer = MemoryMarshal.Read<Ad>(array);
+
+            var result = 0.0;
+            double h = (answer.B - answer.A) / 1000;
+            double h2 = (answer.B - answer.A) * h;
+            for (i = 0; i < 1000; i++)
             {
-                byte[] array = new byte[Unsafe.SizeOf<Ad>()];
-                stream.ReadAsync(array);
-                var answer = MemoryMarshal.Read<Ad>(array);
-
-                var result = 0.0;
-                double h = (answer.B - answer.A) / 1000;
-                double h2 = (answer.B - answer.A) * h;
-                for (i = 0; i < 1000; i++)
-                {
-                    double xi = i * h + h2;
-                    result += -2 * Math.Sin(xi);
-                }
-                result *=  h;
-
-                byte[] spam = new byte[Unsafe.SizeOf<Ud>()];
-
-                var pipa = new Ud { Result = result};
-                MemoryMarshal.Write(spam, ref pipa);
-                await stream.WriteAsync(spam);
+                double xi = i * h + h2;
+                result += -2 * Math.Sin(xi);
             }
+            result *=  h;
+
+            byte[] spam = new byte[Unsafe.SizeOf<Ud>()];
+
+            var pipa = new Ud { Result = result};
+            MemoryMarshal.Write(spam, ref pipa);
+            await stream.WriteAsync(spam);
         }
         catch
         {
